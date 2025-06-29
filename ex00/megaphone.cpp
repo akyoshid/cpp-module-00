7b86ac9ed1d875258d365567ee779ec571e5e776
akyoshid <akyoshid@student.42tokyo.jp>
2025-06-29 19:02:18 +0000

    [RFCT] remove parentheses from return statement & create str_to_upper()

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 15:53:39 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/06/29 18:10:52 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/06/29 18:53:22 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,25 @@
 #include <cctype>
 #include <string>
 
+std::string	str_to_upper(std::string str){
+	for (size_t i = 0; i < str.length(); ++i){
+		str[i] = (char)std::toupper(str[i]);
+	}
+	return str;
+}
+
 int	main(int argc, char *argv[]){
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else{
-		for (int i = 1; i < argc; i++){
-			std::string	str(argv[i]);
-			for (std::string::iterator itr = str.begin();
-				itr != str.end(); ++itr)
-				std::cout << (char)std::toupper(*itr);
-		}
+		for (int i = 1; i < argc; i++)
+			std::cout << str_to_upper(argv[i]);
 	}
 	std::cout << std::endl;
 	if (std::cout.fail() == true){
 		std::cerr << "megaphone: Failed to write to standard output";
 		std::cerr << std::endl;
-		return (1);
+		return 1;
 	}
-	return (0);
+	return 0;
 }
